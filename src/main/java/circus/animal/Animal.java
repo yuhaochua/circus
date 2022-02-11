2e7bcaac3e70460bfaddc3c2b2ddcf31e9d04cf1
yuhaochua <yuhaochua99@gmail.com>
2022-02-11 17:15:45 +0800

    Update Circus and Animal class
    Add comparator to compare animal names
    Use comparator, collections, etc in Circus

--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -2,8 +2,18 @@ package circus.animal;
 
 import circus.Asset;
 
+import java.util.Comparator;
+import java.util.Locale;
+
 public abstract class Animal implements Asset {
 
     public String name;
     public abstract String speak();
+
+    public static Comparator<Animal> AnimalNameComparator = new Comparator<Animal>() {
+        @Override
+        public int compare(Animal animal1, Animal animal2) {
+            return animal1.name.toLowerCase().compareTo(animal2.name.toLowerCase());
+        }
+    };
 }
